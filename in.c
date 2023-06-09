06fa7cfab83bf81c03270f0f05e650fae526d205
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2023-06-09 20:05:59 +0530

    Add files via upload

---
+++ in.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int a = 2;
+    // int b = 43;
+
+    /*for(int i=0;i<6;i++){
+       *** a++;
+    }*/
+
+    int z=100;
+    
+    return 0;
+}
---
+++ out.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int a = 2;
+    
+
+    /*for(int i=0;i<6;i++){
+       *** a++;
+    }*/
+
+    int z=100;
+    
+    return 0;
+}
